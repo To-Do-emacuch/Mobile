6d51fa6605fc9ce35afd55828fc487c617c2aa3f
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-02-08 11:21:08 +0100

    Changed Name app + Add Table Overflow by js

--- www/index.js
+++ www/index.js
@@ -1,3 +1,11 @@
 $(function(){
     $("#btnAddNote").on("click",()=>{alert("ciao")})
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    //css Addings
+    //Element Height = Viewport height - element.offset.top - desired bottom margin
+    console.log($(".myMainTableNoteWrapper").offset().top);
+    console.log(vh)
+    $(".myMainTableNoteWrapper").css("height",(vh -$(".myMainTableNoteWrapper").offset().top));
+    document.getElementsByClassName("myMainTableNoteWrapper")[0].style.setProperty("margin-bottom", "0px", "important")
+    document.getElementsByClassName("myMainTableNoteWrapper")[0].style.setProperty("margin-top", "2rem", "important")
 })
