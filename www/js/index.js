5ef837a7771213fd4a1325c9b560eefc626e02e1
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-02-09 20:16:00 +0100

    Correct static_myAccountNameFontSize - static_myAccountEmailFontSize variable made statics

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,13 @@
 $(function () {
     //CSS
+    let static_myAccountNameFontSize = $(".myAccountName").css("font-size").substring(0, $(".myAccountName").css("font-size").length - 2)
+    let static_myAccountEmailFontSize = $(".myAccountEmail").css("font-size").substring(0, $(".myAccountEmail").css("font-size").length - 2)
     setMyMainTableNoteWrapperHeight(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0));
     //Reduce Text height on scroll
     document.getElementsByClassName('myMainTableNoteWrapper')[0].addEventListener("scroll", () => {
         scrollingTable(
-            $(".myAccountName").css("font-size").substring(0, $(".myAccountName").css("font-size").length - 2),
-            $(".myAccountEmail").css("font-size").substring(0, $(".myAccountEmail").css("font-size").length - 2)
+            static_myAccountNameFontSize,
+            static_myAccountEmailFontSize
         );
     });
     //CSS
