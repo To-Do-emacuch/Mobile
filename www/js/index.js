67a76aa0593bc0d632e6e3330806b78870257ed6
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-02-11 19:06:25 +0100

    wrapping main table rows in apparent divs for swiping feature

--- www/js/index.js
+++ www/js/index.js
@@ -11,4 +11,87 @@ $(function () {
         );
     });
     //CSS
+    $("#myTbody").on("swipe", function () {
+        alert("ye")
+    })
+    const tBody = document.getElementsByClassName('myMainTableNoteWrapper')[0]
+    let touchstartX = 0
+    let touchendX = 0
+
+    let difference = 0;
+    function handleGesture() {
+        difference = touchendX > touchstartX ? touchendX - touchstartX : touchstartX - touchendX
+    }
+    let lastSwiperPositionX = 0;
+    let actualPositionX = 0;
+    tBody.addEventListener('touchstart', e => {
+        lastSwiperPositionX = e.changedTouches[0].screenX
+    })
+
+    tBody.addEventListener('touchend', e => {
+        lastSwiperPositionX = 0;
+        handleGesture()
+    })
+    let first=true;
+    for (let row of $(".myMainTableNoteWrapper").children("div")) {
+        let apparentRow = $(row).children("div")
+        let offsetDiv = row.getBoundingClientRect();
+        if(first)
+        {
+            first = false;
+            baseTop = $(row).css("height")
+        }
+        $(row).css({
+            "width": $(row).css("width"),
+            "height": $(row).css("height")
+        })
+        apparentRow.css({
+            "position": "absolute",
+            "top": (offsetDiv.top-baseTop),
+            "left": offsetDiv.left,
+            "width": $(row).css("width"),
+            "height": $(row).css("height")
+        })
+    }
+    tBody.addEventListener("touchmove", e => {
+        let target = e.target;
+        if (target.tagName.toUpperCase() != "TR" || target.tagName.toUpperCase() != "TD" || target.tagName.toUpperCase() != "H1") {
+            var touch = e.touches[0] || e.changedTouches[0];
+            x = touch.pageX;
+            y = touch.pageY;
+            let tr;
+            console.log($(target).parents()[0])
+            if (target.tagName.toUpperCase() == "TR")
+                tr = target
+            else
+                tr = $(target).parents()[0]
+            console.log(tr)
+            let offsetDiv = tr.getBoundingClientRect();
+            tr = $(tr);
+            console.log(offsetDiv)
+            tr.children("div").css({
+                "top": offsetDiv.top,
+                "left": offsetDiv.left,
+                "width": tr.css("width"),
+                "height": tr.css("height")
+            })
+            let marginSet;
+            if (lastSwiperPositionX < x) {
+                //swiping right
+                marginSet = lastSwiperPositionX + x;/*
+                tr.css("display", "block");
+                tr.css("padding-right", 0);
+                tr.css("padding-left", marginSet)*/
+            }
+            else {
+                //swiping left
+                marginSet = lastSwiperPositionX - x;/*
+                tr.css("display", "block");
+                tr.css("padding-left", 0);
+                tr.css("padding-right", marginSet)*/
+            }
+        }
+        else console.log(target.tagName.toString());
+        lastSwiperPositionX = x;
+    })
 })
